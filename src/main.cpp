4ee90c73c05c7e0cc875386b710c9cb1a55919d3
Raffabello <that.raffaele95bin@gmail.com>
2024-10-05 02:34:17 +0200

    Added the account class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 using namespace std;
+
+class account {
+    
+    private:
+        float balance;
+        string holder;
+    public:
+
+        account(string name) : holder(name) {};
+        
+
+        void getBalance(){
+            cout << balance;
+        }
+
+        void addMoney(){
+            float money;
+
+            cout << "Enter the amount of money:";
+            cin >> money;
+            balance += money;
+        }
+};
+
 int main(void){
 
     return 0;
