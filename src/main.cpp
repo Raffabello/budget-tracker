48ed5900ececb22ac283aee4b57e1b3243ec30a6
Raffabello <142371508+Raffabello@users.noreply.github.com>
2024-10-08 11:02:43 +0900

    Added displayMenu and budgetInit functions

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,36 @@ class account {
         }
 };
 
+void displayMenu(){
+    for(int i = 0; i < 10; i++){cout << "*";}
+    cout << "\n";
+    cout << "Menu:\n" << "quit - close the program\nmenu - display menu\ncreate - create budget account" << endl;
+    for(int i = 0; i < 10; i++){cout << "*";}
+    cout << "\n";
+}
+
+void budgetInit(){
+
+    string userInput;
+    /*If user enters
+        "quit" -> program ends
+    */
+    displayMenu();
+    while(userInput != "quit"){
+        cin >> userInput;
+        if(userInput == "quit"){
+            cout << "Thank you for using budget tracker" << endl;
+        }
+        else if(userInput == "menu"){
+            displayMenu();
+        }
+        else{
+            cout << "This command is not recognized, do you want to see the menu?()" << endl;
+        }
+    }
+
+}
+
 int main(void){
 
     return 0;
